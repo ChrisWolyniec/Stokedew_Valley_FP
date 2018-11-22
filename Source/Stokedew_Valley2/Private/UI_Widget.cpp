d126f876fe89c062e2367f48e9d6b0fca58a27c3
Rhiannon POWELL <p018236h@student.staffs.ac.uk>
2018-11-22 10:56:41 +0000

    UI Widget
    Started creating the UI.

---
+++ Source/Stokedew_Valley2/Private/UI_Widget.cpp
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "UI_Widget.h"
+
+UI_Widget::UI_Widget(const FObjectInitializer& ObjectInitializer) : UUserWidget(ObjectInitializer)
+{
+
+}
+
+UI_Widget::~UI_Widget()
+{
+
+}
+
+void UI_Widget::NativeConstruct()
+{
+	Super::NativeConstruct();
+}
---
+++ Source/Stokedew_Valley2/Public/UI_Widget.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Runtime/UMG/Public/UMG.h"
+#include "Runtime/UMG/Public/UMGStyle.h"
+#include "Runtime/UMG/Public/IUMGModule.h"
+#include "Runtime/UMG/Public/Blueprint/UserWidget.h"
+
+/**
+ * 
+// */
+class STOKEDEW_VALLEY2_API UI_Widget : public UUserWidget
+{
+public:
+	UI_Widget(const FObjectInitializer& ObjectInitializer);
+	~UI_Widget();
+
+
+	virtual void NativeConstruct() override;
+};
